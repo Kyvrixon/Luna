6fa2f5c35a1ec883c7d9ffb42628116baa1c7b1f
Kyvrixon <ky@kyvrixon.dev>
2026-03-12 18:57:14 +1100

    fix(afk): fixed new line character in `.join()` call

--- src/apps/bot/core/modules/afk/events/messageCreate.ts
+++ src/apps/bot/core/modules/afk/events/messageCreate.ts
@@ -76,7 +76,7 @@ export default new DiscordEvent({
 										},
 									)} ago`,
 									`   **Reason:** ${targetAfk.message}`,
-								].join("\v"),
+								].join("\n"),
 							),
 						),
 					],
